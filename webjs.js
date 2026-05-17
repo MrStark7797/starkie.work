74c1b414b5c48fe1cef00f18c88c58ef64787467
MrStark7797 <starkiewill@gmail.com>
2026-05-17 16:34:32 +0100

    photo gal

--- webjs.js
+++ webjs.js
@@ -1,7 +1,30 @@
-const accordion = document.getElementsByClassName('container');
+let slideIndex = 1;
+showSlides(slideIndex);
 
-for (var i=0; i<accordion.length; i++) {
-  accordion[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-  })
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
+window.plusSlides = plusSlides;
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+window.currentSlide = currentSlide;
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+} 
